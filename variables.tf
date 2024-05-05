ae09a3916d703583c2ec060b98eb1101d78208ef
12sergey12 <12sergey12@example.com>
2024-05-05 20:28:10 +0700

    images

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "yandex_cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "yandex_folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "ubuntu-2004-lts" {
+  default = "fd8cnj92ad0th7m7krqh"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
